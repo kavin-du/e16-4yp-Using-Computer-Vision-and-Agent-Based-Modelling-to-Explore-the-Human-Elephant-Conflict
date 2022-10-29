ecf35698c56670db123780b0082675af6333cad3
kavin-du <ukdkcm@gmail.com>
2022-10-29 18:16:56 +0530

    prevent duplicate downloading datasets and pretrained models

--- code/Swin-Transformer-Semantic-Segmentation/script3_prepare_datasets.sh
+++ code/Swin-Transformer-Semantic-Segmentation/script3_prepare_datasets.sh
@@ -6,15 +6,15 @@ FOLDER_NAME=dataset_zips
 
 mkdir $FOLDER_NAME
 
-wget https://zenodo.org/record/5706578/files/Test.zip -P $FOLDER_NAME
+wget -nc https://zenodo.org/record/5706578/files/Test.zip -P $FOLDER_NAME
 
-wget https://zenodo.org/record/5706578/files/Train.zip -P $FOLDER_NAME
+wget -nc https://zenodo.org/record/5706578/files/Train.zip -P $FOLDER_NAME
 
-wget https://zenodo.org/record/5706578/files/Val.zip -P $FOLDER_NAME
+wget -nc https://zenodo.org/record/5706578/files/Val.zip -P $FOLDER_NAME
 
 
 echo "Downloading the pretrained model..."
-wget https://github.com/SwinTransformer/storage/releases/download/v1.0.8/swin_tiny_patch4_window7_224_22k.pth
+wget -nc https://github.com/SwinTransformer/storage/releases/download/v1.0.8/swin_tiny_patch4_window7_224_22k.pth
 
 # converting the datasets
 python tools/convert_datasets/loveda.py $FOLDER_NAME
