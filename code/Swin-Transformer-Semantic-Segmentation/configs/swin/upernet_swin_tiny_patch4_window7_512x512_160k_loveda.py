a18e993b856f5ba332d4454fbd506a14c275f78a
kavin-du <ukdkcm@gmail.com>
2022-11-14 15:25:04 +0530

    updated scripts for swin according to mmsegmentation

--- code/Swin-Transformer-Semantic-Segmentation/configs/swin/upernet_swin_tiny_patch4_window7_512x512_160k_loveda.py
+++ code/Swin-Transformer-Semantic-Segmentation/configs/swin/upernet_swin_tiny_patch4_window7_512x512_160k_loveda.py
@@ -4,22 +4,20 @@ _base_ = [
 ]
 model = dict(
     backbone=dict(
-        embed_dim=96,
+        embed_dims=96,
         depths=[2, 2, 6, 2],
         num_heads=[3, 6, 12, 24],
         window_size=7,
-        ape=False,
         drop_path_rate=0.3,
         patch_norm=True,
-        use_checkpoint=False
     ),
     decode_head=dict(
         in_channels=[96, 192, 384, 768],
-        num_classes=150
+        num_classes=7
     ),
     auxiliary_head=dict(
         in_channels=384,
-        num_classes=150
+        num_classes=7
     ))
 
 # AdamW optimizer, no weight decay for position embedding & layer norm in backbone
